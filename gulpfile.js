7009d3d13111f99f09bc433140b9e0fbee877e3b
celulaRHdev <44373891+celulaRHdev@users.noreply.github.com>
2019-08-08 13:13:23 -0300

    remove dependences

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,8 @@
 const { src, dest, parallel } = require('gulp');
 var gulp          = require('gulp'),
-    babel         = require('gulp-babel');
-    opn           = require('opn'),
     concat        = require('gulp-concat'),
     minifyCSS     = require('gulp-minify-css'),
-    rename        = require('gulp-rename'),
     uglify        = require('gulp-uglify'),
-    minifyHTML    = require('gulp-minify-html'),
-    replaceHTML   = require('gulp-html-replace'),
-    rimraf        = require('gulp-rimraf'),
-    ignore        = require('gulp-ignore'),
-    zip           = require('gulp-zip'),
-    checkFileSize = require('gulp-check-filesize'),
-    watch         = require('gulp-watch'),
 
     serveDir = './src',
     distPaths = {
